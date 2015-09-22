1bf219466700b3ac0beed0ccd4302be40d45282c
Laurent Declercq <l.declercq@nuxwin.com>
2015-09-22 20:09:21 +0200

    Added: JS code (in progress) [ci skip]

--- gui/themes/default/assets/js/aps_standard.js
+++ gui/themes/default/assets/js/aps_standard.js
@@ -17,8 +17,84 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-var iMSCP = iMSCP || { };
+var iMSCP = iMSCP || {};
+iMSCP.apsStandard = angular.module("apsStandard", ["ngTable", "ngResource", "jQueryUI"]);
 
-iMSCP.apsStandard = angular.module("apsStandard", [ "ngTable", "ngResource" ]);
+// APS standard package resource
+(function () {
+	"use strict";
+	iMSCP.apsStandard.config(['$httpProvider', function ($httpProvider) {
+		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	}]);
 
+	iMSCP.apsStandard.factory("PackageResource", ['$resource', function ($resource) {
+		return $resource("aps_packages.php", { id: '@id' }, {
+			update: { method: 'PUT' }
+		});
+	}]);
+})();
 
+// APS standard package controller
+(function () {
+	"use strict";
+
+	iMSCP.apsStandard.controller("PackageController", ['$scope', 'NgTableParams', 'PackageResource', function (
+		$scope, NgTableParams, PackageResource
+	) {
+		$scope.packages = PackageResource.query(function (packages) {
+			$scope.tableParams = new NgTableParams({ count: 5 }, {
+				dataset: packages, counts: [ 5, 10, 25, 30, 50 ]
+			});
+		});
+
+		// Lock/Unlock package
+		$scope.changeStatus = function (newStatus, prevStatus) {
+			this.package.status = newStatus;
+			this.package.$update().catch(function (response) {
+				response.config.data.status = prevStatus;
+			});
+		};
+	}]);
+})();
+
+iMSCP.jQueryUI = angular.module("jQueryUI", []);
+
+// jQuery directives
+(function () {
+	"use strict";
+	iMSCP.jQueryUI.directive('jqButton', function () {
+		return {
+			restrict: 'E', // says that this directive is only for html elements
+			replace: true,
+			template: '<input type="button">',
+			link: function (scope, element, attrs) {
+				element.button();
+			}
+		};
+	});
+})();
+
+// Ajax loader
+(function($) {
+	"use strict";
+	iMSCP.apsStandard.directive('ajaxLoader', ['$http' , function ($http) {
+		return {
+			restrict: 'A',
+			scope: false,
+			link: function (scope, elm, attrs) {
+				scope.isLoading = function () {
+					return $http.pendingRequests.length > 0;
+				};
+
+				scope.$watch(scope.isLoading, function (v) {
+					if(v) {
+						$("body").addClass("loading");
+					} else {
+						$("body").removeClass("loading");
+						elm.show();
+					}
+				});
+			}
+		}
+	}]);
+})(jQuery);
