38d439d368699596a1e1f4262dd89a8a6175e54f
Laurent Declercq <l.declercq@nuxwin.com>
2015-10-03 11:12:34 +0200

    in progress... [ci skip]

--- gui/public/admin/aps_packages.php
+++ gui/public/admin/aps_packages.php
@@ -21,7 +21,6 @@
 namespace iMSCP\ApsStandard;
 
 use iMSCP\ApsStandard\Controller\Package as PackageController;
-use iMSCP\ApsStandard\Entity\Package as PackageEntity;
 use iMSCP_Events_Aggregator as EventManager;
 use iMSCP_Events as Events;
 use iMSCP_pTemplate as TemplateEngine;
@@ -33,25 +32,9 @@ $eventManager = EventManager::getInstance();
 $eventManager->dispatch(Events::onAdminScriptStart);
 check_login('admin');
 
-if (is_xhr()) { // Dispatches the XHR request based on HTTP verbs
+if (is_xhr()) { // Handle the XHR request
 	$controller = new PackageController($eventManager);
-
-	switch ($_SERVER['REQUEST_METHOD']) {
-		case 'GET': // Return packages list
-			$controller->indexAction();
-			break;
-		case 'PUT': // Change (lock/unlock) package status
-			$payload = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
-			if (is_array($payload)) {
-				$controller->changeStatus(new PackageEntity($payload));
-			}
-			break;
-		case 'POST':
-			// Trigger package index update
-			$controller->updateIndexAction();
-	}
-
-	$controller->sendJsonResponse(400);
+	$controller->handleRequest();
 }
 
 $tpl = new TemplateEngine();
@@ -59,22 +42,31 @@ $tpl->define_dynamic(array(
 	'layout' => 'shared/layouts/ui.tpl',
 	'page' => 'shared/partials/aps_standard/aps_packages.tpl',
 	'page_message' => 'layout',
-	'aps_package_entry' => 'page'
+	'admin_only_block' => 'page'
 ));
 
 $tpl->assign(array(
 	'TR_PAGE_TITLE' => tohtml(tr('Admin / APS Standard / Packages'), 'htmlAttr'),
+	'TR_GLOBAL_SEARCH' => tohtml(tr('Global search', 'htmlAttr')),
 	'TR_DETAILS' => tohtml(tr('Details')),
 	'TR_CATEGORY' => tohtml(tr('Category')),
 	'TR_VENDOR' => tohtml(tr('Vendor')),
 	'TR_CERTIFIED' => tohtml(tr('Certified')),
 	'TR_LOCK' => tohtml(tr('Lock'), 'htmlAttr'),
 	'TR_UNLOCK' => tohtml(tr('Unlock'), 'htmlAttr'),
-	'TR_UPDATE_PACKAGE_INDEX' => tohtml(tr('Update package index'))
+	'TR_TOTAL_PACKAGES' => tohtml(tr('Total packages')),
+	'TR_UPDATE_PACKAGE_INDEX' => tohtml(tr('Update package index')),
+	'PAGE_MESSAGE' => '' // Not needed
 ));
 
+$eventManager->registerListener('onGetJsTranslations', function ($e) {
+	$e->getParam('translations')->core['aps'] = array(
+		'no_package_available' => tr('No package available. You should update package index.'),
+		'update_in_progress' => tr('Update of package index is in progress. This task can take several minutes.')
+	);
+});
+
 generateNavigation($tpl);
-generatePageMessage($tpl);
 
 $tpl->parse('LAYOUT_CONTENT', 'page');
 $eventManager->dispatch(Events::onAdminScriptEnd, array('templateEngine' => $tpl));
