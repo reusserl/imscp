15b3bf13f50ae0e321d6d7140085c41f7a4f9aee
Laurent Declercq <l.declercq@nuxwin.com>
2015-10-09 21:07:08 +0200

    Added ORM service factory Added doctrine CLI script [ci skip]

---
+++ gui/bin/doctrine.php
@@ -0,0 +1,5 @@
+<?php
+
+// Include i-MSCP core library
+require_once '../library/imscp-lib.php';
+require_once iMSCP_Registry::get('config')->CACHE_DATA_DIR . '/packages/vendor/doctrine/orm/bin/doctrine.php';
---
+++ gui/library/iMSCP/Service/ORMServiceFactory.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2015 by Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace iMSCP\Service;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Class ORMServiceFactory
+ * @package iMSCP\Service
+ */
+class ORMServiceFactory
+{
+	/**
+	 * @var EntityManager
+	*/
+	static $entityManager;
+
+	/**
+	 * Create doctrine entity manager service
+	 *
+	 * @throws \Doctrine\ORM\ORMException
+	 * @throws \iMSCP_Exception
+	 * @param \PDO $pdo
+	 * @return EntityManager
+	 */
+	public static function create(\PDO $pdo)
+	{
+		if (static::$entityManager === null) {
+			$config = \iMSCP_Registry::get('config');
+			$config = Setup::createAnnotationMetadataConfiguration(
+				array(LIBRARY_PATH . "/iMSCP/Entity"), $config['DEBUG'], CACHE_PATH . '/orm_proxy'
+			);
+
+			//$pdo->setAttribute(\PDO::ATTR_STATEMENT_CLASS, array('Doctrine\DBAL\Driver\PDOStatement', array()));
+			static::$entityManager = $entityManager = EntityManager::create(
+				array('driver' => 'pdo_mysql', 'pdo' => $pdo), $config
+			);
+		}
+
+		return static::$entityManager;
+	}
+}
