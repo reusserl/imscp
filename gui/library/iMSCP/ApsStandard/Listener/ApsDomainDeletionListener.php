d93084794b93323c9c513852339b33121ddd4fd0
Laurent Declercq <l.declercq@nuxwin.com>
2015-11-13 02:50:49 +0100

    Added: ApsDomainDeletionListener aggregate [ci skip]

---
+++ gui/library/iMSCP/ApsStandard/Listener/ApsDomainDeletionListener.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2015 by Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace iMSCP\ApsStandard\Listener;
+
+use Doctrine\ORM\EntityManager;
+use iMSCP_Events_ListenerAggregateInterface as ListenerAggregateInterface;
+use iMSCP_Events_Manager_Interface as EventManager;
+use iMSCP_Events as Events;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use iMSCP_Events_Event as Event;
+
+/**
+ * Class DomainDeletionListener
+ *
+ * @package iMSCP\ApsStandard
+ */
+class ApsDomainDeletionListener implements ListenerAggregateInterface, ServiceLocatorAwareInterface
+{
+	/**
+	 * @var ServiceLocatorInterface
+	 */
+	protected $serviceLocator;
+
+	/**
+	 * @var array
+	 */
+	protected $listeners = array();
+
+	/**
+	 * {@inheritdoc
+	 */
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+	{
+		$this->serviceLocator = $serviceLocator;
+	}
+
+	/**
+	 * {@inheritdoc
+	 */
+	public function getServiceLocator()
+	{
+		return $this->serviceLocator;
+	}
+
+	/**
+	 * {@inheritdoc
+	 */
+	public function register(EventManager $eventManager, $priority = 1)
+	{
+		$this->listeners = $eventManager->registerListener(
+			array(
+				Events::onBeforeDeleteDomainAlias,
+				Events::onBeforeDeleteSubdomain
+			),
+			$this,
+			$priority
+		);
+	}
+
+	/**
+	 * {@inheritdoc
+	 */
+	public function unregister(EventManager $eventManager)
+	{
+		foreach ($this->listeners as $listener) {
+			$eventManager->unregisterListener($listener);
+		}
+
+		$this->listeners = array();
+	}
+
+	/**
+	 * Deletes APS application instances which belong to the deleted domain
+	 *
+	 * @param Event $event
+	 * @return void
+	 */
+	public function __invoke(Event $event)
+	{
+		/** @var EntityManager $em */
+		$em = $this->getServiceLocator()->get('EntityManager');
+		$em->getConfiguration()->addEntityNamespace('Aps', '\\iMSCP\\ApsStandard\\Entity\\');
+		$q = $em->createQuery('delete from Aps:ApsInstance i where i.domainId = ?0 and i.domainType = ?1');
+
+		switch ($event->getName()) {
+			case Events::onBeforeDeleteDomainAlias:
+				$q->execute(array($event->getParam('domainAliasId'), 'als'));
+				break;
+			case Events::onBeforeDeleteSubdomain:
+				$q->execute(array($event->getParam('domainAliasId'), $event->getParam('type')));
+		}
+	}
+}
