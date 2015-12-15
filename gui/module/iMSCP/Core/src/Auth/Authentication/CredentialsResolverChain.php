cae6216febab8bd31591ab8848f7523ecf63ba6b
Laurent Declercq <l.declercq@nuxwin.com>
2015-12-16 00:54:32 +0100

    Core\Auth: Added CredentialsResolverChain resolver [ci skip]

---
+++ gui/module/iMSCP/Core/src/Auth/Authentication/CredentialsResolverChain.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2015 by Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace iMSCP\Core\Auth\Authentication;
+
+/**
+ * Class CredentialsResolverChain
+ * @package iMSCP\Core\Auth\Authentication
+ */
+class CredentialsResolverChain implements CredentialsResolverChainInterface
+{
+    /**
+     * @var CredentialsResolverInterface[]
+     */
+    protected $resolvers;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addResolver(CredentialsResolverInterface $resolver)
+    {
+        $this->resolvers[] = $resolver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolve($identity, $credential)
+    {
+        foreach ($this->resolvers as $resolver) {
+            $credentials = $resolver->resolve($identity, $credential);
+
+            if (false !== $credentials) {
+                return $credentials;
+            }
+        }
+
+        return false;
+    }
+}
